90bb4ecf4aad9da2650e2e33e6423981cd980553
artsbentley <artsbentley@gmail.com>
2024-11-12 21:26:37 +0100

    feat: removed partials and refactor

--- database.py
+++ database.py
@@ -1,10 +1,13 @@
 import model
 from typing import List
+from sqlite3 import Connection, Cursor, DatabaseError
+from contextlib import contextmanager
+from loguru import logger
 
 
 class Database:
-    def __init__(self, sqlite_connection, logger):
-        self.conn = sqlite_connection
+    def __init__(self, connection: Connection):
+        self.conn = connection
         self.logger = logger
         self.create_tables()
 
@@ -15,49 +18,64 @@ class Database:
 
         return factory
 
+    # @contextmanager
+    # def get_cursor(self):
+    #     cursor = self.conn.cursor()
+    #     try:
+    #         yield cursor
+    #         self.conn.commit()
+    #     except Exception as e:
+    #         self.conn.rollback()
+    #         self.logger.error(f"Database operation failed: {str(e)}")
+    #         raise DatabaseError(f"Database operation failed: {str(e)}") from e
+    #     finally:
+    #         cursor.close()
+
     def create_tables(self):
-        cursor = self.conn.cursor()
-        cursor.execute(
+        with self.conn:
+            cursor = self.conn.cursor()
+            cursor.execute(
+                """
+            CREATE TABLE IF NOT EXISTS users (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT,
+                age INTEGER,
+                email TEXT,
+                UNIQUE (name)
+            )
             """
-        CREATE TABLE IF NOT EXISTS users (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT,
-            age INTEGER,
-            email TEXT
-        )
-        """
-        )
-        cursor.execute(
+            )
+            cursor.execute(
+                """
+            CREATE TABLE IF NOT EXISTS products (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT,
+                price REAL,
+                in_stock INTEGER
+            )
             """
-        CREATE TABLE IF NOT EXISTS products (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT,
-            price REAL,
-            in_stock INTEGER
-        )
-        """
-        )
-        self.conn.commit()
-        self.logger.info("Tables created or verified successfully")
+            )
+            self.conn.commit()
+            self.logger.info("Tables created or verified successfully")
 
     def insert_user(self, user: model.User):
-        cursor = self.conn.cursor()
         assert isinstance(user, model.User), "input must be of type 'User'"
-        cursor.execute(
-            "INSERT INTO users (name, age, email) VALUES (?, ?, ?)",
-            (user.name, user.age, user.email),
-        )
-        self.conn.commit()
-        self.logger.info(f"Inserted user: {user}")
+        with self.conn:
+            cursor = self.conn.cursor()
+            cursor.execute(
+                "INSERT INTO users (name, age, email) VALUES (?, ?, ?)",
+                (user.name, user.age, user.email),
+            )
+            self.logger.info(f"Inserted user: {user}")
 
     def get_all_users(self) -> List[model.User]:
-        cursor = self.conn.cursor()
-        cursor.row_factory = self.model_factory(model.User)
-        cursor.execute("SELECT * FROM users")
-        rows = cursor.fetchall()
+        with self.conn:
+            cursor = self.conn.cursor()
+            cursor.row_factory = self.model_factory(model.User)
+            cursor.execute("SELECT * FROM users")
+            rows = cursor.fetchall()
 
-        assert isinstance(
-            rows[1], model.User
-        ), "the function must return a list of Users"
-        self.logger.info("Fetched all users")
-        return rows
+            assert not rows or isinstance(
+                rows[0], model.User
+            ), "The function must return a list of Users"
+            return rows
--- main.py
+++ main.py
@@ -2,22 +2,62 @@ import sqlite3
 import atexit
 import model
 import database
-from loguru import logger
+# from loguru import logger
+
+from jinja2 import Template
+from jinja2_fragments.fastapi import Jinja2Blocks
+
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse, FileResponse
+
+# from fastapi.templating import Jinja2Templates
 
 
-def main():
-    conn = sqlite3.connect("app.db")
-    atexit.register(lambda: conn.close())
+app = FastAPI()
+templates = Jinja2Blocks(directory="templates")
 
-    db = database.Database(conn, logger)
+conn = sqlite3.connect("app.db")
+atexit.register(lambda: conn.close())
 
-    user = model.User(name="Arno", age=28, email="arnoarts@hotmail.com")
-    db.insert_user(user)
-
-    users = db.get_all_users()
-    for user in users:
-        print(user.id, user.age)
+db = database.Database(conn)
 
 
-if __name__ == "__main__":
-    main()
+@app.get("/favicon.ico")
+async def get_favicon():
+    return FileResponse("static/favicon.ico")
+
+
+@app.get("/")
+async def root(request: Request):
+    context = {"users": db.get_all_users()}
+    return templates.TemplateResponse(
+        "index.html", {"request": request, "context": context}
+    )
+
+
+@app.post("/user")
+async def create_user(
+    request: Request,
+    name: str = Form(...),
+    age: int = Form(...),
+    email: str = Form(...),
+):
+    context = {}
+    try:
+        user = model.User(name=name, age=age, email=email)
+        db.insert_user(user)
+        context["users"] = db.get_all_users()
+        context["success_message"] = "Added user successfully"
+    except Exception as e:
+        context["error_message"] = str(e)
+        context["users"] = db.get_all_users()
+
+    return templates.TemplateResponse(
+        "index.html",
+        {"request": request, "context": context},
+        block_name="table",
+    )
+
+
+# if __name__ == "__main__":
+#     main()
